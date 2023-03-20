c13703bfb95845be1cfbe7f5c2032e3b235a8a74
shihchiehlin2003 <a0981930700@gmail.com>
2023-03-20 14:46:40 +0800

    6.
    1.將所有textbox加入keyup
    2.每個副程式裡面加入strInput=txtxxx.Text;
    3.txtxxx=Text="";

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -48,20 +48,102 @@ namespace temp
             // 判斷式，如果能夠以double.TryParse成功轉型，那才做數值的計算
             if (double.TryParse(strInput, out douOutput) == true)
             {
-                txtm.Text = string.Format("{0:0.##########}", douOutput / 100);
-                txtkm.Text = string.Format("{0:0.##########}", douOutput / 100000);
-                txtin.Text = string.Format("{0:0.##########}", douOutput / 2.54);
-                txtft.Text = string.Format("{0:0.##########}", douOutput / 30.48);
-                txtyard.Text = string.Format("{0:0.##########}", douOutput / 91.44);
+                caculateAnswer(0, douOutput);
             }
             else
             {
                 // 如果無法轉型，則是在說明文字中顯示錯誤訊息，並且將txtCM文字框清除
                 txtinfo.Text = "請輸入數字";
                 txtcm.Text = "";
+            }           
+        }
+
+        private void txtm_KeyUp(object sender, KeyEventArgs e)
+        {
+            strInput = txtm.Text;
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(1, douOutput * 100);
+            }
+            else
+            {
+                txtinfo.Text = "請輸入數字";
+                txtm.Text = "";
             }
         }
 
+        // 設計一個單位轉換計算的函式，沒有回傳值，設計兩個參數，1.類別參數、2.數值參數
+        void caculateAnswer(int _kind, double _value)
+        {
+            if (_kind != 0)
+                txtcm.Text = string.Format("{0:0.##########}", _value);
+            if (_kind != 1)
+                txtm.Text = string.Format("{0:0.##########}", _value / 100);
+            if (_kind != 2)
+                txtkm.Text = string.Format("{0:0.##########}", _value / 100000);
+            if (_kind != 3)
+                txtin.Text = string.Format("{0:0.##########}", _value / 2.54);
+            if (_kind != 4)
+                txtft.Text = string.Format("{0:0.##########}", _value / 30.48);
+            if (_kind != 5)
+                txtyard.Text = string.Format("{0:0.##########}", _value / 91.44);
+        }
+
+        private void txtkm_KeyUp(object sender, KeyEventArgs e)
+        {
+            strInput = txtkm.Text;
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(2, douOutput * 100000);
+            }
+            else
+            {
+                txtinfo.Text = "請輸入數字";
+                txtkm.Text = "";
+            }
+        }
+
+        private void txtin_KeyUp(object sender, KeyEventArgs e)
+        {
+            strInput = txtin.Text;
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(3, douOutput * 2.54);
+            }
+            else
+            {
+                txtinfo.Text = "請輸入數字";
+                txtin.Text = "";
+            }
+        }
+
+        private void txtft_KeyUp(object sender, KeyEventArgs e)
+        {
+            strInput = txtft.Text;
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(4, douOutput * 30.48);
+            }
+            else
+            {
+                txtinfo.Text = "請輸入數字";
+                txtft.Text = "";
+            }
+        }
+
+        private void txtyard_KeyUp(object sender, KeyEventArgs e)
+        {
+            strInput = txtyard.Text;
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                caculateAnswer(5, douOutput * 91.44);
+            }
+            else
+            {
+                txtinfo.Text = "請輸入數字";
+                txtyard.Text = "";
+            }
+        }
     }
 
     }
